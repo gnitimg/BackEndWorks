1d784c2504ac960e7fcf2b825393197ce22c59ac
gnitimg <135971567+gnitimg@users.noreply.github.com>
2025-02-02 13:22:38 +0800

    Upload Version v1.0.1

---
+++ 图书管理系统/v1.0.1/user/User.java
@@ -0,0 +1,13 @@
+package user;
+
+public class User {
+    protected String username;
+
+    public User(String username) {
+        this.username = username;
+    }
+
+    //public void checkBook(ArrayList<Book> list, String bookname) {
+        //new Operate().checkBook(list, bookname);
+    //}
+}
